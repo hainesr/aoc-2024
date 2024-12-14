42459916f901523f0a40f79043805852cdbbcf4e
Robert Haines <rhaines@manchester.ac.uk>
2024-12-14 14:36:57 +0000

    Solution for day 14, part 2.

--- lib/aoc2024/days/restroom_redoubt.rb
+++ lib/aoc2024/days/restroom_redoubt.rb
@@ -19,6 +19,21 @@ module AOC2024
       safety_factor(moved, max_x: max_x, max_y: max_y)
     end
 
+    def part2
+      range_start = 7000
+
+      # Now I know where the Easter Egg is, I've squashed the range that is
+      # checked to 2000. But this is how I found the answer, using an
+      # original range of 1000 to 10000. Even starting at 1000 only takes
+      # 1.1 seconds to run on my machine.
+      scores = (range_start..9000).map do |secs|
+        moved = @input.map { |state| move(state, secs: secs) }
+        safety_factor(moved)
+      end
+
+      scores.index(scores.min) + range_start
+    end
+
     def safety_factor(positions, max_x: 101, max_y: 103)
       mid_x = (max_x / 2)
       mid_y = (max_y / 2)
@@ -39,6 +54,17 @@ module AOC2024
       y = pos[1] + (vec[1] * secs)
 
       [x % max_x, y % max_y]
+    end
+
+    # This method is not used in solving the puzzle, but is useful for
+    # checking the result of part 2.
+    def plot(positions, max_x: 101, max_y: 103)
+      max_y.times do |y|
+        max_x.times do |x|
+          print positions.include?([x, y]) ? '#' : '.'
+        end
+        puts
+      end
     end
 
     def read_input(input)
