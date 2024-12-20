4857246e2fb8b7e8fd39e79eb164552b04e1a783
Robert Haines <rhaines@manchester.ac.uk>
2024-12-20 13:04:42 +0000

    Solution for day 19, part 2.

--- lib/aoc2024/days/linen_layout.rb
+++ lib/aoc2024/days/linen_layout.rb
@@ -12,11 +12,29 @@ module AOC2024
   class LinenLayout < Day
     def setup(input = read_input_file.chomp)
       @patterns, @designs = read_input(input)
+
+      # Need a global cache as some designs will be subsets of others so
+      # we can avoid recalculating those bits of larger designs.
+      @counts = {}
     end
 
     def part1
       regex = Regexp.new("^(#{@patterns.join('|')})+$")
       @designs.count { |d| regex.match?(d) }
+    end
+
+    def part2
+      @designs.sum { |d| count(d) }
+    end
+
+    def count(design)
+      return 1 if design.empty?
+
+      # Return the count for this design (or part of a design) if we have
+      # already calculated it. If not, calculate it now by recursion.
+      @counts[design] ||= @patterns.select { |d| design.start_with?(d) }.sum do |d|
+        count(design[d.size..])
+      end
     end
 
     def read_input(input)
--- test/days/linen_layout_test.rb
+++ test/days/linen_layout_test.rb
@@ -34,9 +34,28 @@ class AOC2024::LinenLayoutTest < Minitest::Test
     assert_equal(%w[brwrr bggr gbbr rrbgbr ubwu bwurrg brgr bbrgwb], designs)
   end
 
+  def test_count
+    @ll.setup(INPUT)
+
+    assert_equal(2, @ll.count('brwrr'))
+    assert_equal(1, @ll.count('bggr'))
+    assert_equal(4, @ll.count('gbbr'))
+    assert_equal(6, @ll.count('rrbgbr'))
+    assert_equal(0, @ll.count('ubwu'))
+    assert_equal(1, @ll.count('bwurrg'))
+    assert_equal(2, @ll.count('brgr'))
+    assert_equal(0, @ll.count('bbrgwb'))
+  end
+
   def test_part1
     @ll.setup(INPUT)
 
     assert_equal(6, @ll.part1)
+  end
+
+  def test_part2
+    @ll.setup(INPUT)
+
+    assert_equal(16, @ll.part2)
   end
 end
